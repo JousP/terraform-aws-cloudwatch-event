203c735e054e0c02288e70e55c166d985deccee5
Julie "Jous" Paillusseau <julie.paillusseau@nbs-system.com>
2020-02-28 10:23:41 +0100

    add missing outputs

--- examples/custom-usage/main.tf
+++ examples/custom-usage/main.tf
@@ -29,3 +29,43 @@ output "cloudwatch_event_pattern_arn" {
   description = "The Amazon Resource Name (ARN) of the rule."
   value       = module.cloudwatch_event_pattern.arn
 }
+
+output "cloudwatch_event_pattern_id" {
+  description = "The name of the rule."
+  value       = module.cloudwatch_event_pattern.id
+}
+
+output "cloudwatch_event_pattern_name" {
+  description = "The rule's name."
+  value       = module.cloudwatch_event_pattern.name
+}
+
+output "cloudwatch_event_pattern_schedule_expression" {
+  description = "The scheduling expression."
+  value       = module.cloudwatch_event_pattern.schedule_expression
+}
+
+output "cloudwatch_event_pattern_event_pattern" {
+  description = "Event pattern described a JSON object."
+  value       = module.cloudwatch_event_pattern.event_pattern
+}
+
+output "cloudwatch_event_pattern_description" {
+  description = "The description of the rule."
+  value       = module.cloudwatch_event_pattern.description
+}
+
+output "cloudwatch_event_pattern_role_arn" {
+  description = "The Amazon Resource Name (ARN) associated with the role that is used for target invocation."
+  value       = module.cloudwatch_event_pattern.role_arn
+}
+
+output "cloudwatch_event_pattern_is_enabled" {
+  description = "Whether the rule should be enabled."
+  value       = module.cloudwatch_event_pattern.is_enabled
+}
+
+output "cloudwatch_event_pattern_tags" {
+  description = "A mapping of tags to assign to the resource."
+  value       = module.cloudwatch_event_pattern.tags
+}
--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,48 @@ output "arn" {
   description = "The Amazon Resource Name (ARN) of the rule."
   value       = element(concat(aws_cloudwatch_event_rule.main.*.arn, [""]), 0)
 }
+
+output "id" {
+  description = "The name of the rule."
+  value       = element(concat(aws_cloudwatch_event_rule.main.*.id, [""]), 0)
+}
+
+output "name" {
+  description = "The rule's name."
+  value       = element(concat(aws_cloudwatch_event_rule.main.*.name, [""]), 0)
+}
+
+# output "name_prefix" {
+#   description = "The rule's name. Conflicts with name."
+#   value       = element(concat(aws_cloudwatch_event_rule.main.*.name_prefix, [""]), 0)
+# }
+
+output "schedule_expression" {
+  description = "The scheduling expression."
+  value       = element(concat(aws_cloudwatch_event_rule.main.*.schedule_expression, [""]), 0)
+}
+
+output "event_pattern" {
+  description = "Event pattern described a JSON object."
+  value       = element(concat(aws_cloudwatch_event_rule.main.*.event_pattern, [""]), 0)
+}
+
+output "description" {
+  description = "The description of the rule."
+  value       = element(concat(aws_cloudwatch_event_rule.main.*.description, [""]), 0)
+}
+
+output "role_arn" {
+  description = "The Amazon Resource Name (ARN) associated with the role that is used for target invocation."
+  value       = element(concat(aws_cloudwatch_event_rule.main.*.role_arn, [""]), 0)
+}
+
+output "is_enabled" {
+  description = "Whether the rule should be enabled."
+  value       = element(concat(aws_cloudwatch_event_rule.main.*.is_enabled, [""]), 0)
+}
+
+output "tags" {
+  description = "A mapping of tags to assign to the resource."
+  value       = element(concat(aws_cloudwatch_event_rule.main.*.tags, [""]), 0)
+}
