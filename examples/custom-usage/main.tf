a145e1142b64a529378647c10dd51c545ed685e1
Julie "Jous" Paillusseau <jous@akiros.io>
2022-09-28 09:57:40 +0200

    v3.1.0: remove experiments. Experiment module_variable_optional_attrs is no longer available. The final feature corresponding to this experiment differs from the experimental form and is available in the Terraform language from Terraform v1.3.0 onwards.

--- examples/custom-usage/main.tf
+++ examples/custom-usage/main.tf
@@ -14,11 +14,14 @@ locals {
   PATTERN
 }
 
+resource "aws_sns_topic" "notifications" {
+  name = "notifications"
+}
+
 module "cloudwatch_event_pattern" {
   source        = "JousP/cloudwatch-event/aws"
-  version       = "~> 3.0"
   name          = "custom-rule-pattern"
   event_pattern = local.event_pattern
   description   = "Custom Test"
-  target_arn    = "arn:aws:iam::123456789012:user/Development/product_1234/"
+  target_arn    = aws_sns_topic.notifications.arn
 }
